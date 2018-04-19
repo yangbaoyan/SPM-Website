1b3c10d007df0bfd4ef8122b1d3feb14f4bff03a
pppppap <1026516401@qq.com>
2018-04-19 21:21:52 +0800

    first

--- src/test/java/com/example/controller/IndexController.java
+++ src/test/java/com/example/controller/IndexController.java
@@ -1,4 +1,4 @@
-package com.example.controller.com.example;
+package com.example.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
