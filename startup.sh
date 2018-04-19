2ac2761b0cb11b33368e41b81916ab0898e7d348
pppppap <1026516401@qq.com>
2018-04-19 21:43:09 +0800

    first

--- startup.sh
+++
@@ -1,6 +0,0 @@
-ehco "授予当前用户权限"
-chmod 777 /deploy_cmp/cmp-0.0.1-SNAPSHOT.jar
-echo "执行....."
-cd /deploy_cmp
-java -jar cmp-0.0.1-SNAPSHOT.jar
-echo "**********************cmp on  jenkins started*************************"
