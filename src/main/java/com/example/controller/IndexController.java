cb7af2941f476a35ab255bbf53811c2abc9263d3
pppppap <1026516401@qq.com>
2018-05-16 23:08:38 +0800

    html -> ftl

--- src/main/java/com/example/controller/IndexController.java
+++ src/main/java/com/example/controller/IndexController.java
@@ -1,20 +1,21 @@
 package com.example.controller;
 
 import com.example.service.StudentService;
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class IndexController {
     @Autowired
     StudentService studentService;
 
-    @RequestMapping("home")
-    public ModelAndView home() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("index.html");
+    @RequestMapping("/home")
+    public ModelAndView home(HttpServletRequest httpServletRequest, ModelAndView modelAndView) {
+        modelAndView.setViewName("index");
         return modelAndView;
     }
+
 }
