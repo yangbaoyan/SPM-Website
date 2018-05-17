2475234b236933af539c904b4889d58a7ab4322b
pppppap <1026516401@qq.com>
2018-05-17 08:54:37 +0800

    add getStudent controller

--- src/main/java/com/example/controller/IndexController.java
+++ src/main/java/com/example/controller/IndexController.java
@@ -1,10 +1,12 @@
 package com.example.controller;
 
+import com.example.entity.Student;
 import com.example.service.StudentService;
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -12,10 +14,9 @@ public class IndexController {
     @Autowired
     StudentService studentService;
 
-    @RequestMapping("/home")
-    public ModelAndView home(HttpServletRequest httpServletRequest, ModelAndView modelAndView) {
-        modelAndView.setViewName("index");
-        return modelAndView;
+    @ResponseBody
+    public Student getStudent(HttpServletRequest httpServletRequest) {
+        String id = httpServletRequest.getParameter("id");
+        return studentService.find(id);
     }
-
 }
